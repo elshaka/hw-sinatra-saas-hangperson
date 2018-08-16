21bcb5f953d22e9f86963f1f65ae2b84ef956cac
Eleazar Meza <bairesdev_emeza@iseatz.com>
2018-08-16 00:07:04 -0400

    Implemented HangpersonGame class

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,34 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+  attr_reader :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(guess)
+    raise ArgumentError if (guess.nil? || guess.empty? || !(guess =~ /\w/))
+    guess.downcase!
+    if valid = @word.include?(guess) && !@guesses.include?(guess)
+      @guesses += guess
+    else
+      @wrong_guesses += guess unless @wrong_guesses.include?(guess)
+    end
+    valid
+  end
+
+  def word_with_guesses
+    @word.gsub(/./) { |c| @guesses.include?(c) ? c : '-' }
+  end
+
+  def check_win_or_lose
+    return :lose if @wrong_guesses.size == 7
+    return :win if @word == word_with_guesses
+    return :play
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
